7b761343bf02319613e8b0ba2220a5c80f9c67ae
Austin Smith <asmith2@novetta.com>
2020-04-04 23:24:04 -0400

    Unit tests for testing which poker hands win.

--- poker.js
+++ poker.js
@@ -1,15 +1,15 @@
 const Card = (r, s) => ({rank: r, suit: s});
 
 const HandRankings = Object.freeze({
-    HIGH: 1,
-    PAIR: 2,
-    TWO_PAIR: 3,
-    THREE_OF_KIND: 4,
-    STRAIGHT: 5,
-    FLUSH: 6,
-    FULL_HOUSE: 7,
-    FOUR_OF_KIND: 8,
-    STRAIGHT_FLUSH: 9
+    HIGH: 0,
+    PAIR: 1,
+    TWO_PAIR: 2,
+    THREE_OF_KIND: 3,
+    STRAIGHT: 4,
+    FLUSH: 5,
+    FULL_HOUSE: 6,
+    FOUR_OF_KIND: 7,
+    STRAIGHT_FLUSH: 8
 });
 
 const Ranks = Object.freeze({
@@ -20,7 +20,11 @@ const Ranks = Object.freeze({
     ACE: 13
 });
 
-const getHighCard = (hand) => Math.max(hand.map(card => card.rank));
+const getHighCard = (hand) => Math.max(...hand.map(card => card.rank));
+const getNextHighestCards = (hand, n) => {
+    hand.sort((a,b) => b.rank - a.rank);
+    return hand.splice(1, n+1);
+};
 
 const count = (elem, arry) => {
     let count = 0;
@@ -32,12 +36,36 @@ const count = (elem, arry) => {
     return count;
 };
 
+/* Returns the element that occurs n times in array. */
+const occurs = (n, arry) => {
+    let counts = new Map();
+    for (const elem of arry) {
+        const count = counts.get(elem);
+        if (count) {
+            counts.set(elem, count+1);
+        }
+        else {
+            counts.set(elem, 1);
+        }
+    }
+    for (const [key, value] of counts.entries()) {
+        if (value === n) {
+            return key;
+        }
+    }
+    return undefined;
+};
+
+/* Removes all instances of element from the array. */
+const remove = (elem, array) => array.filter(e => e != elem);
+
 const handRanks = (hand) => hand.map(card => card.rank);
 const handSuits = (hand) => hand.map(card => card.suit);
 
-/* These tests must be applied to hand in decreasing order of strength i.e straight flushes before four of kind... etc
- * This is because each function checks for only minimum viability. For example, isThreeOfKind will return true
- * if it is passed a Full House because it detects three of the same cards.
+/* These tests must be applied to hand in decreasing order of strength i.e straight flushes 
+ * before four of kind... etc. This is because each function checks for only minimum viability. 
+ * For example, isThreeOfKind will return true if it is passed a Full House because it detects 
+ * three of the same cards.
 */
 const isPair = (hand) => count(2, handRanks(hand).map(card => count(card, handRanks(hand)))) === 2; 
 const isTwoPair = (hand) => count(2, handRanks(hand).map(card => count(card, handRanks(hand)))) === 4;
@@ -64,9 +92,92 @@ const isFullHouse = (hand) => isPair(hand) && isThreeOfKind(hand);
 const isFourOfKind = (hand) => Math.max(...handRanks(hand).map(card => count(card, handRanks(hand)))) === 4;
 const isStraightFlush = (hand) => isStraight(hand) && isFlush(hand);
 
-const classifyHand = (hand) => undefined;
+// TYPE, STRENGTH, TIEBREAKER?
+// HIGH, K 
+// PAIR, 2, A
+// TWOPAIR, 8J, A
+// THREE OF KIND, 8, K
+// STRAIGHT, 9 
+// FLUSH, J
+// FULLHOUSE, 10,K
+// FOUR KIND, 3, A
+// STRAIGHTFLUSH, A
+const handStrength = (hand) => {
+    if (isStraightFlush(hand)) {
+        return { type: HandRankings.STRAIGHT_FLUSH, strength: getHighCard(hand) };
+    }
+    else if (isFourOfKind(hand)) {
+        return { type: HandRankings.FOUR_OF_KIND, strength: occurs(4, handRanks(hand)), tiebreak: occurs(1, handRanks(hand)) };
+    }
+    else if (isFullHouse(hand)) {
+        return { type: HandRankings.FULL_HOUSE, strength: occurs(3, handRanks(hand)), tiebreak: occurs(2, handRanks(hand)) };
+    }
+    else if (isFlush(hand)) {
+        return { type: HandRankings.FLUSH, strength: getHighCard(hand) };
+    }
+    else if (isStraight(hand)) { 
+        return { type: HandRankings.STRAIGHT, strength: getHighCard(hand) };
+    }
+    else if (isThreeOfKind(hand)) {
+        return { type: HandRankings.THREE_OF_KIND, strength: occurs(3, handRanks(hand)), tiebreak: getNextHighestCards(occurs(3, handRanks(hand)), 2) };
+    }
+    else if (isTwoPair(hand)) {
+        const pair1 = occurs(2, handRanks(hand));
+        const pair2 = occurs(2, remove(pair1, handRanks(hand)));
+        const bigPair = Math.max(pair1, pair2);
+        const smallPair = Math.min(pair1, pair2);
+        return { 
+            type: HandRankings.TWO_PAIR, 
+            strength: [bigPair, smallPair], 
+            tiebreak: occurs(1, handRanks(hand)) 
+        };
 
-const handStrength = (hand) => [classifyHand(hand), getHighCard(hand)];
+    }
+    else if (isPair(hand)) {
+        return { type: HandRankings.PAIR, strength: occurs(2, handRanks(hand)), tiebreak: getNextHighestCards(remove(occurs(2, handRanks(hand)), handRanks(hand)), 3) };
+    }
+    else {
+        return { type: HandRankings.HIGH, strength: getHighCard(hand), tiebreak: getNextHighestCards(remove(getHighCard(hand), handRanks(hand)), 4) };
+    }
+};
+
+exports.doesHandWin = (a, b) => {
+    const A_WINS = 1;
+    const B_WINS = 2;
+    const SPLIT = 0;
+    const aStrength = handStrength(a);
+    const bStrength = handStrength(b);
+    if (aStrength.type > bStrength.type) {
+        return A_WINS;
+    }
+    else if (aStrength.type === bStrength.type) {
+        if (aStrength.strength > bStrength.strength) {
+            return A_WINS;
+        }
+        else if (aStrength.strength === bStrength.strength) {
+            if (aStrength.tiebreak && bStrength.tiebreak) {
+                if (aStrength.tiebreak > bStrength.tiebreak) {
+                    return A_WINS;
+                }
+                else if (aStrength.tiebreak === bStrength.tiebreak) {
+                    return SPLIT;
+                }
+                else {
+                    return B_WINS;
+                }
+            }
+            else {
+                return SPLIT;
+            }
+        }
+        else {
+            return B_WINS;
+        }
+    }
+    else {
+        return B_WINS;
+    }
+};
 
 const createDeck = () => {
     const deck = [];
--- test/poker.test.js
+++ test/poker.test.js
@@ -81,3 +81,100 @@ describe('Straight Flush Test', () => {
         assert.equal(poker.isStraightFlush(mockHand([3, 4, 5, 6, 7], true)), true);
     });
 });
+
+describe('High Hand Winning', () => {
+    it('High Hand Basic Win', () => {
+        assert.equal(poker.doesHandWin(mockHand([3,2,4,6,13]), mockHand([5, 1, 4, 10, 12])), 1);
+    });
+    it('High Hand Split Pot', () => {
+        assert.equal(poker.doesHandWin(mockHand([13, 10, 9, 8, 5]), mockHand([13, 10, 9, 8, 5])));
+    });
+});
+
+describe('Pair Winning', () => {
+    it('Beats High Hand', () => {
+        assert.equal(poker.doesHandWin(mockHand([2, 2, 10, 8, 9]), mockHand([13, 10, 9, 7, 3])), 1);
+    });
+    it('Higher Pair Wins', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 8, 3, 4]), mockHand([10, 10, 7, 3, 4])), 1);
+    });
+    it('Kicker Plays', () => {
+        assert.equal(poker.doesHandWin(mockHand([10, 10, 13, 11, 2]), mockHand([10, 10, 9, 11, 2])), 1);
+    });
+    it('Split Pot', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 10, 8, 4]), mockHand([12, 12, 10, 8, 4])), 0);
+    });
+});
+
+describe('Two Pair Winning', () => {
+    it('Beats Lower Hands', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 6, 6, 13]), mockHand([13, 9, 3, 4, 5])), 1);
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 6, 6, 13]), mockHand([13, 13, 4, 5, 6])), 1);
+    });
+    it('Beats Lower Two Pair', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 10, 10, 13]), mockHand([10, 10, 9, 9, 13])), 1);
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 10, 10, 13]), mockHand([12, 12, 9, 9, 13])), 1);
+    });
+    it('Kicker Plays', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 10, 10, 13]), mockHand([12, 12, 10, 10, 9])), 1);
+    });
+    it('Split Pot', () => {
+        assert.equal(poker.doesHandWin(mockHand([12, 12, 10, 10, 13]), mockHand([12, 12, 10, 10, 13])), 3);
+    });
+});
+
+describe('Three of Kind Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats Lower Three of Kinds', () => {
+    });
+    it('Kicker Plays', () => {
+    });
+    it ('Split Pot', () => {
+    });
+});
+
+describe('Straight Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats Lower Straights', () => {
+    });
+    it('Split Pot', () => {
+    });
+});
+
+describe('Flush Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats Lower Flushes', () => {
+    });
+    it('Split Pot', () => {
+    });
+});
+
+describe('Fullhouse Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats Lower FullHouses', () => {
+    });
+    it('Split Pot', () => {
+    });
+});
+
+describe('Four of Kind Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats Lower Four-of-Kinds', () => {
+    });
+    it('Split Pot', () => {
+    });
+});
+
+describe('Straight Flushes Winning', () => {
+    it('Beats Lower Hands', () => {
+    });
+    it('Beats lower straight flushes', () => {
+    });
+    it('Split Pot', () => {
+    });
+});
