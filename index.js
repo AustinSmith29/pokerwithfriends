236b441458369d7284486f2c3392507acb7fcf74
Austin Smith <asmith2@novetta.com>
2020-04-13 12:42:02 -0400

    wip: landing page

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.use('/scripts/socket.io', express.static(__dirname + '/node_modules/socket.io-client/dist/'));
+app.use('/scripts/phaser', express.static(__dirname + '/node_modules/phaser/dist/'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
